a66293dd77fb2447a528405de2d812e013629fe0
Hasanga Ranasinghe <dinithhasangare@gmail.com>
2025-11-30 15:39:53 +0530

    Add Service2 application class with Eureka client support

---
+++ service2/src/main/java/com/example/service2/Service2Application.java
@@ -0,0 +1,13 @@
+package com.example.service2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient  // This enables Eureka client functionality for this microservice
+public class Service2Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Service2Application.class, args);
+    }
+}
